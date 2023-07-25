04d2322b1156081d9e5ada3c2648fa13ce6602de
Alex <alex@hartcode.com>
2023-07-24 19:50:32 -0700

    Refactor routes

--- src/api/routes.js
+++ src/api/routes.js
@@ -2,16 +2,34 @@ import auth from '@/auth';
 import health from '@/health';
 import {verifyAuthMiddleWare} from '@/verifyAuth';
 import express from 'express';
+import {ROUTES_AUTH, ROUTES_HEALTH} = '../constants'
+
+
+const authenticatedRoutesConfig = {
+  middleware:[verifyAuthMiddleWare],
+  routes:[{route: ROUTES_AUTH, function: auth}]
+};
+
+const unauthenticatedRoutesConfig = {
+  middleware:[],
+  routes:[{route: ROUTES_HEALTH, function: health}]
+};
 
 export const authenticatedRoutes = function() {
-  const router = express.Router();
-  router.use(verifyAuthMiddleWare);
-  router.get('/auth', auth);
-  return router;
+  return createRoutingFromConfig(authenticatedRoutesConfig)
 };
 
 export const unauthenticatedRoutes = function() {
-  const router = express.Router();
-  router.get('/health', health);
-  return router;
+  return createRoutingFromConfig(unauthenticatedRoutesConfig)
 };
+
+const createRoutingFromConfig = function(config) {
+  const router = express.Router();
+  config?.middleware?.forEach(middleware => {
+    router.use(middleware);
+  });
+  config?.routes?.forEach(route => {
+    router.get(route.route, route.function);
+  });
+  return router;
+}
--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,8 @@
 
 export const ROUTES_BASE = '/';
 export const ROUTES_API = ROUTES_BASE + 'api';
+export const ROUTES_AUTH = '/auth';
+export const ROUTES_HEALTH = '/health';
 
 export const ENV_PRODUCTION = 'production';
 
