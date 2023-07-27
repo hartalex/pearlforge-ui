0538a149ce1bf698f2a2783c50a91de8d6953fb0
Alex <alex@hartcode.com>
2023-07-27 13:22:26 -0700

    WIP add some proptypes

--- src/client/components/title.jsx
+++ src/client/components/title.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Title = ({ text }) => <h1>{text}</h1>;
+
+Title.propTypes = {
+	text: PropTypes.string,
+};
+
+Title.defaultProps = {
+  text: ''
+};
 
 export default Title;
